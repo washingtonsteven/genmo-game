1f16ad87fcae077c6a5e07e0f981895351b5f390
Steven Washington <washington.steven@gmail.com>
2021-02-28 18:33:47 -0500

    Updating genmo version to get inventory functionality

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import { Genmo } from "@esaevian/genmo-v2";
-import { useEffect, useState, useRef } from "react";
+import { useEffect, useState, useRef, cloneElement } from "react";
 import Engine from "./story/Engine.json";
 
 const emptyPassage = {
@@ -25,13 +25,41 @@ const GenmoProvider = (props) => {
     });
     setCurrentPassage(genmoRef.current.getCurrentPassage());
   }, []);
+  const allProps = {
+    ...props,
+    currentPassage,
+    followLink,
+    genmo: genmoRef.current,
+  };
+  return <div>{cloneElement(props.children, { ...allProps })}</div>;
+};
+
+const GenmoGame = ({ currentPassage, followLink, genmo }) => {
+  const linkClick = (link) => {
+    return () => {
+      followLink(link);
+    };
+  };
+
   return (
     <div>
-      {props.children({
-        currentPassage,
-        followLink,
-        genmo: genmoRef.current,
-      })}
+      <div className="text">{currentPassage.passageText}</div>
+      {currentPassage.links && (
+        <div className="links">
+          {currentPassage.links.map((link) => (
+            <button key={link.pid} onClick={linkClick(link)}>
+              {link.name}
+            </button>
+          ))}
+        </div>
+      )}
+      {genmo && (
+        <div className="state">
+          <pre>
+            <code>{JSON.stringify(genmo.state.data, null, 2)}</code>
+          </pre>
+        </div>
+      )}
     </div>
   );
 };
@@ -39,34 +67,7 @@ const GenmoProvider = (props) => {
 const App = () => {
   return (
     <GenmoProvider>
-      {({ currentPassage, followLink, genmo }) => {
-        const linkClick = (link) => {
-          return () => {
-            followLink(link);
-          };
-        };
-        return (
-          <div>
-            <div className="text">{currentPassage.passageText}</div>
-            {currentPassage.links && (
-              <div className="links">
-                {currentPassage.links.map((link) => (
-                  <button key={link.pid} onClick={linkClick(link)}>
-                    {link.name}
-                  </button>
-                ))}
-              </div>
-            )}
-            {genmo && (
-              <div className="state">
-                <pre>
-                  <code>{JSON.stringify(genmo.state.data, null, 2)}</code>
-                </pre>
-              </div>
-            )}
-          </div>
-        );
-      }}
+      <GenmoGame />
     </GenmoProvider>
   );
 };
