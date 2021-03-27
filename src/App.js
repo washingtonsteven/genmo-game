7220fad7da96278b334172243327414a3c8fdc7f
Steven Washington <washington.steven@gmail.com>
2021-03-27 12:58:06 -0400

    Adding story selector + Bunker

--- src/App.js
+++ src/App.js
@@ -1,12 +1,98 @@
-import Engine from "./story/Engine.json";
+import Engine from "./stories/Engine.json";
+import Bunker from "./stories/Bunker.json";
 import { GenmoProvider } from "./GenmoProvider";
 import { GenmoGame } from "./GenmoGame";
+import {
+  AppBar,
+  Button,
+  Container,
+  createMuiTheme,
+  IconButton,
+  makeStyles,
+  ThemeProvider,
+  Toolbar,
+  Typography,
+} from "@material-ui/core";
+import { Fragment, useState } from "react";
+import { ArrowBack } from "@material-ui/icons";
+
+const stories = [Engine, Bunker];
+
+const darkTheme = createMuiTheme({
+  palette: {
+    type: "dark",
+  },
+});
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    padding: theme.spacing(3),
+  },
+  storyButton: {
+    display: "block",
+    width: "100%",
+    marginBottom: theme.spacing(2),
+  },
+}));
+
+const StorySelector = ({ stories, onStorySelected, classes }) => {
+  return (
+    <Container maxWidth="sm" className={classes.root}>
+      <h1>Select A Story</h1>
+      {stories.map((storyData) => (
+        <Button
+          key={storyData.name}
+          onClick={() => onStorySelected(storyData)}
+          variant="contained"
+          color="primary"
+          className={classes.storyButton}
+        >
+          {storyData.name}
+        </Button>
+      ))}
+    </Container>
+  );
+};
+
+const GenmoToolbar = ({ storyName, onBackClicked }) => {
+  return (
+    <AppBar position="static">
+      <Toolbar>
+        <IconButton edge="start" onClick={onBackClicked}>
+          <ArrowBack />
+        </IconButton>
+        <Typography variant="h6">{storyName}</Typography>
+      </Toolbar>
+    </AppBar>
+  );
+};
 
 const App = () => {
+  const [selectedStory, setSelectedStory] = useState(null);
+  const classes = useStyles();
+
   return (
-    <GenmoProvider storyData={Engine}>
-      <GenmoGame />
-    </GenmoProvider>
+    <ThemeProvider theme={darkTheme}>
+      {selectedStory ? (
+        <Fragment>
+          <GenmoToolbar
+            storyName={selectedStory.name}
+            onBackClicked={() => setSelectedStory(null)}
+          />
+          <GenmoProvider storyData={selectedStory}>
+            <GenmoGame classes={classes} />
+          </GenmoProvider>
+        </Fragment>
+      ) : (
+        <StorySelector
+          classes={classes}
+          stories={stories}
+          onStorySelected={(story) =>
+            setSelectedStory(stories.find((s) => s.name === story.name))
+          }
+        />
+      )}
+    </ThemeProvider>
   );
 };
 
--- src/GenmoGame.js
+++ src/GenmoGame.js
@@ -1,11 +1,4 @@
-import {
-  Container,
-  createMuiTheme,
-  CssBaseline,
-  Grid,
-  makeStyles,
-  ThemeProvider,
-} from "@material-ui/core";
+import { Container, CssBaseline, Grid, makeStyles } from "@material-ui/core";
 import React from "react";
 import { PassageText } from "./elements/PassageText";
 import { Links } from "./elements/Links";
@@ -13,16 +6,7 @@ import { Stats } from "./elements/Stats";
 import { Inventory } from "./elements/Inventory";
 import { allPromptsResponded } from "./utils/promptFunctions";
 
-const darkTheme = createMuiTheme({
-  palette: {
-    type: "dark",
-  },
-});
-
 const useStyles = makeStyles((theme) => ({
-  root: {
-    padding: theme.spacing(3),
-  },
   sidebar: {
     "& > *": {
       margin: theme.spacing(1),
@@ -31,6 +15,7 @@ const useStyles = makeStyles((theme) => ({
 }));
 
 export const GenmoGame = ({
+  classes: containerClasses,
   currentPassage,
   followLink,
   respondToPrompt,
@@ -38,33 +23,31 @@ export const GenmoGame = ({
 }) => {
   const classes = useStyles();
   return (
-    <ThemeProvider theme={darkTheme}>
-      <Container maxWidth="lg" className={classes.root}>
-        <CssBaseline />
-        <Grid container spacing={2}>
-          {/* Left Side */}
-          <Grid item xs={12} sm={7} lg={9}>
-            <Grid container spacing={2}>
-              <PassageText
-                passage={currentPassage}
-                onPromptResponded={respondToPrompt}
-              />
-              <Links
-                links={currentPassage.links}
-                onLinkClicked={followLink}
-                disabled={!allPromptsResponded(currentPassage)}
-              />
-            </Grid>
-          </Grid>
-          {/* Right Side */}
-          <Grid item xs={12} sm={5} lg={3}>
-            <Grid container spacing={2} className={classes.sidebar}>
-              <Stats data={genmo.getData()} />
-              <Inventory inventory={genmo.getInventory()} />
-            </Grid>
+    <Container maxWidth="lg" className={containerClasses.root}>
+      <CssBaseline />
+      <Grid container spacing={2}>
+        {/* Left Side */}
+        <Grid item xs={12} sm={7} lg={9}>
+          <Grid container spacing={2}>
+            <PassageText
+              passage={currentPassage}
+              onPromptResponded={respondToPrompt}
+            />
+            <Links
+              links={currentPassage.links}
+              onLinkClicked={followLink}
+              disabled={!allPromptsResponded(currentPassage)}
+            />
           </Grid>
         </Grid>
-      </Container>
-    </ThemeProvider>
+        {/* Right Side */}
+        <Grid item xs={12} sm={5} lg={3}>
+          <Grid container spacing={2} className={classes.sidebar}>
+            <Stats data={genmo.getData()} />
+            <Inventory inventory={genmo.getInventory()} />
+          </Grid>
+        </Grid>
+      </Grid>
+    </Container>
   );
 };
